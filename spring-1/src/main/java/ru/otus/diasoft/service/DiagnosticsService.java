fd3c658f349fb8e4e80cc0527ee29d6689499f1d
Mikhail <poker.kmp@gmail.com>
2025-10-01 14:11:58 +0300

    Рефакторинг

--- spring-1/src/main/java/ru/otus/diasoft/service/DiagnosticsService.java
+++ spring-1/src/main/java/ru/otus/diasoft/service/DiagnosticsService.java
@@ -1,34 +1,7 @@
 package ru.otus.diasoft.service;
 
-import ru.otus.diasoft.model.Question;
+public interface DiagnosticsService {
 
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-
-/**
- * Сервис для проведения тестирования (диагностики)
- */
-public class DiagnosticsService {
-
-    /**
-     * Запуск тестирования
-     */
-    public static void runTest(List<Question> questions) {
-        int numberCorrectAnswers = 0;
-        try (Scanner scanner = new Scanner(System.in)) {
-            for (Question question : questions) {
-                System.out.println(question.getQuestion());
-                for (Map.Entry<Integer, String> answerOption : question.getAnswerOption().entrySet()) {
-                    System.out.println(String.format("%s - %s", answerOption.getKey(), answerOption.getValue()));
-                }
-                Integer answer = scanner.nextInt();
-                if (question.getCorrectAnswer().equals(answer)) {
-                    numberCorrectAnswers++;
-                }
-            }
-        }
-        System.out.println(String.format("Количество правильных ответов: %s / %s", numberCorrectAnswers, questions.size()));
-    }
+    void runDiagnostics();
 
 }
